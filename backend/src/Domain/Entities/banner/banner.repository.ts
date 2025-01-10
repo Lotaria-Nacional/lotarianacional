0cade873ae0a52707e094242065223381e23774d
Paulods0 <pauloluguenda0@gmail.com>
2025-01-10 03:55:38 +0100

    add banners features

--- backend/src/Domain/Entities/banner/banner.repository.ts
+++ backend/src/Domain/Entities/banner/banner.repository.ts
@@ -4,6 +4,7 @@ export interface IBannerRespository {
   save(banner: Banner): Promise<void>;
   getAll(): Promise<Banner[]>;
   getFirst(): Promise<Banner | null>;
+  getById(id: string): Promise<Banner | null>;
   update(id: string, data: Partial<BannerProps>): Promise<void>;
   delete(id: string): Promise<void>;
 }
