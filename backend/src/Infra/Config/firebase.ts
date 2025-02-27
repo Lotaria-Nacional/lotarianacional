7270f8c9aa8894f7c699d77248cc2e68d1714c59
Paulods0 <pauloluguenda0@gmail.com>
2025-02-27 08:42:37 +0100

    Salvando alterações antes do pull

---
+++ backend/src/Infra/Config/firebase.ts
@@ -0,0 +1,9 @@
+import { v2 as firebase } from "cloudinary"
+
+firebase.config({
+  cloud_name: "df9aom75p",
+  api_key: "755384124166594",
+  api_secret: "LOfnvYq1RF4zl8iFFl-Wah8O_5w",
+})
+
+export { firebase }
---
+++ backend/src/Infra/Services/ImageUpload/FirebaseUploadService.ts
@@ -0,0 +1,46 @@
+import { firebase } from "../../Config/firebase"
+import { CloudinaryFileType, IFileUpload } from "../../../Domain/services/fileUpload.service.interface"
+
+export class FirebaseUploadService implements IFileUpload {
+  async upload(file: string, folder: string, type:CloudinaryFileType): Promise<string> {
+    console.log("Chegou no Firebase Service")
+    return new Promise((resolve, reject) => {
+      if (typeof file === "string" && file.endsWith(".zip")) {
+        return reject(new Error("Arquivos ZIP não são suportados"))
+      }
+      firebase.uploader
+        .upload_stream(
+          {
+            folder,
+            resource_type: type,
+          },
+          (error, result) => {
+            if (error) {
+              console.log("Erro ao fazer upload do arquivo no cloudinary")
+              return reject(error)
+            }
+            resolve(result?.secure_url || "")
+          }
+        )
+        .end(file)
+    })
+  }
+  async delete(publicId: string): Promise<void> {
+    try {
+      const result = await firebase.uploader.destroy(publicId, {
+        resource_type: "raw", 
+      })
+      if (result.result === "ok") {
+        console.log("Ficheiro deletado com sucesso.")
+      } else {
+        console.log(
+          "Erro ao tentar deletar o ficheiro. Resposta do Cloudinary:",
+          result
+        )
+      }
+    } catch (error) {
+      console.log("Erro ao deletar o arquivo no Cloudinary: ", error)
+      throw error
+    }
+  }
+}
