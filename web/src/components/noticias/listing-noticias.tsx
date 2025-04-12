27c3846a44969a7e0857691b5d90315a52fd2b45
Paulods0 <pauloluguenda0@gmail.com>
2025-04-12 10:33:03 +0100

    fix: view 3 news on home page

--- web/src/components/noticias/listing-noticias.tsx
+++ web/src/components/noticias/listing-noticias.tsx
@@ -1,10 +1,12 @@
-import NoticiaCard from "./noticia-card";
-import EmptyState from "../common/empty-state";
-import { useNews } from "@/hooks/api/query/useNews";
-import ListingNewsSkeleton from "./listing-news-skeleton";
+import NoticiaCard from "./noticia-card"
+import EmptyState from "../common/empty-state"
+import { useNews } from "@/hooks/api/query/useNews"
+import ListingNewsSkeleton from "./listing-news-skeleton"
 
 const ListingNoticias = () => {
-  const { isLoading, news } = useNews(1);
+  const { isLoading, news } = useNews(1)
+
+  const lastNews = news.data.slice(0, 3)
 
   return (
     <>
@@ -12,7 +14,7 @@ const ListingNoticias = () => {
         <ListingNewsSkeleton />
       ) : news && news?.data.length > 0 ? (
         <div className="grid w-full grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 lg:gap-3">
-          {news.data.slice(-3).map((data) => (
+          {lastNews.map((data) => (
             <NoticiaCard key={data.id} noticia={data} />
           ))}
         </div>
@@ -20,7 +22,7 @@ const ListingNoticias = () => {
         <EmptyState className="h-[100px]" message="Não há notícias ainda." />
       )}
     </>
-  );
-};
+  )
+}
 
-export default ListingNoticias;
+export default ListingNoticias
