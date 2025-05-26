77b637d2187811cadffdf008e43f9b55dbf60176
Paulods0 <pauloluguenda0@gmail.com>
2025-05-26 13:05:00 +0100

    refactor: add banner button

--- dashboard/src/features/banners/components/banner-tab-content.tsx
+++ dashboard/src/features/banners/components/banner-tab-content.tsx
@@ -16,15 +16,25 @@ export default function BannerTabContent({ value, image }: Props) {
         </div>
         <div className="flex items-center w-full gap-2">
           <Button className="flex-1" variant="grayDarker">
-            Alterar
+            <label
+              htmlFor="btn"
+              className="text-white cursor-pointer w-fit flex items-center justify-center"
+            >
+              Alterar
+              <input id="btn" type="file" accept="image/*" className="hidden" />
+            </label>
           </Button>
           <Button className="flex-1">Eliminar</Button>
         </div>
       </div>
-      <Button variant="yellow" className="!text-black">
+      <label
+        htmlFor="btn"
+        className="!text-black cursor-pointer w-fit bg-LT-YELLOW px-3 py-2 rounded-lg flex items-center justify-center gap-2"
+      >
         <Plus size={14} />
         Adicionar banner
-      </Button>
+        <input id="btn" type="file" accept="image/*" className="hidden" />
+      </label>
     </TabsContent>
   );
 }
