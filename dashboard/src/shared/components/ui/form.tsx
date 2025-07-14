2651aa07eabcc512562fe39c7f59ea7924c5e316
Paulods0 <pauloluguenda0@gmail.com>
2025-07-14 17:13:17 +0100

    feat: jobs

--- dashboard/src/shared/components/ui/form.tsx
+++ dashboard/src/shared/components/ui/form.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import * as React from "react"
 import * as LabelPrimitive from "@radix-ui/react-label"
 import { Slot } from "@radix-ui/react-slot"
@@ -12,7 +14,7 @@ import {
 } from "react-hook-form"
 
 import { cn } from "@/lib/utils"
-import { Label } from "@/components/ui/label"
+import { Label } from "@//shared/components/ui/label"
 
 const Form = FormProvider
 
